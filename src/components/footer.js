54e8e9cb39a7f15d302cfe804126ac8b93edbe6a
MichaelDHuy <dachuy87@gmail.com>
2023-07-07 02:30:04 -0700

    first codes

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <hr />
+      <p>&copy; 2023 Your Name. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/projects.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <ul>
+        <li>
+          <a href="https://project1.com">Project 1</a>
+        </li>
+        <li>
+          <a href="https://project2.com">Project 2</a>
+        </li>
+        <li>
+          <a href="https://project3.com">Project 3</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Projects;
